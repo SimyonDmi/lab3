5265ae657d7bb6f7a4e45509d78893a461f88b6e
Saeid <2011_sa@mail.ru>
2018-06-13 10:33:37 +0700

    plus

--- TRPO3/Program.cs
+++ TRPO3/Program.cs
@@ -10,7 +10,12 @@ namespace TRPO3
     {
         static void Main(string[] args)
         {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
 
+            Console.WriteLine("{0}+{1}={2}", a, b, a + b);
+
+            Console.ReadKey();
         }
     }
 }
